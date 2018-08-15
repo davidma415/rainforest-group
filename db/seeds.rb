47bd4f717abe2f2407a8174f92f19c11cc86b4ef
David Ma <davidma415@gmail.com>
2018-08-15 11:05:57 -0400

    Added some products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: "Pouch", description: "A pouch for your water", price_in_cents: 500)
+
+Product.create(name: "Boots", description: "Made for walking", price_in_cents: 10000)
+
+Product.create(name: "Benwah Balls", description: "Kegel exercisers", price_in_cents: 1000)
