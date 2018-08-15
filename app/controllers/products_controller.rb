5401fcc34b9363899021923e4141b2ce2f97690c
Billy Davidson <billy.davids52@gmail.com>
2018-08-15 12:04:42 -0400

    error message created

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,28 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents].to_i
+    if @product.save
+      redirect_to products_url
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
 end
