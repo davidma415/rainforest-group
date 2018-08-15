6bba4713635b429593f0262ffd2f61254c600152
David Ma <davidma415@gmail.com>
2018-08-15 12:32:16 -0400

    Finished edit forms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -16,7 +16,8 @@ class ProductsController < ApplicationController
     @product = Product.new
     @product.name = params[:product][:name]
     @product.description = params[:product][:description]
-    @product.price_in_cents = params[:product][:price_in_cents].to_i
+    @product.price_in_cents = params[:product][:price_in_cents]
+
     if @product.save
       redirect_to products_url
     else
@@ -26,6 +27,20 @@ class ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      redirect_to products_url
+    else
+      render :edit
+    end
   end
 
   def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
 
   validates :name, :description, :price_in_cents, presence: true
   validates :price_in_cents, numericality: { only_integer: true}
+  
 end
